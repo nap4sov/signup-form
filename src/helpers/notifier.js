85f6e3249fb753b1b19d79c79be2aaab95344c14
Roman <g4vr0m@gmail.com>
2022-10-20 10:21:31 +0300

    add fix

--- src/helpers/notifier.js
+++ src/helpers/notifier.js
@@ -1,7 +1,6 @@
 import { toast } from 'react-toastify';
 
 export const showError = error => {
-    console.log(Array.isArray(error.response.data.error));
     const errorMessage = Array.isArray(error.response.data.error)
         ? error.response.data.error[0].message
         : error.response.data.error;
