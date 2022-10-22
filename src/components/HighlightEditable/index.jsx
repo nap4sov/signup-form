ff3b8708fd43136166a72f80fac1c48423580fc7
Roman <g4vr0m@gmail.com>
2022-10-22 15:44:55 +0300

    add toggle editable

--- src/components/HighlightEditable/index.jsx
+++ src/components/HighlightEditable/index.jsx
@@ -6,31 +6,35 @@ import { highlightWords } from '../../helpers/highlightWords';
 export const HighlightEditable = () => {
     const [search, setSearch] = useState('');
     const [text, setText] = useState('');
+    const [editable, setEditable] = useState(true);
 
-    const handleInputChange = ({ target }) => {
+    const handleBlur = ({ target }) => {
         setText(target.textContent);
+        setEditable(false);
     };
 
-    const handleDoubleClick = () => {
+    const handleDoubleClick = ({ target }) => {
+        if (!text) setText(target.textContent);
         const selection = document.getSelection();
         setSearch(selection.toString());
+        setEditable(false);
+    };
+
+    const handleResetClick = () => {
+        setText('');
+        setSearch('');
+        setEditable(true);
     };
 
     return (
         <Wrapper>
-            <button
-                onClick={() => {
-                    setText('');
-                    setSearch('');
-                }}
-            >
-                clear
-            </button>
+            <button onClick={handleResetClick}>Reset</button>
 
             <EditableField
                 type="plain"
-                handleInputChange={handleInputChange}
+                editable={editable}
                 handleDoubleClick={handleDoubleClick}
+                handleBlur={handleBlur}
                 value={search ? highlightWords({ text, search }) : text}
             />
         </Wrapper>
